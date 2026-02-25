5fac4be14ca9879634534ce5be7135aa04c38045
m48331 <maksshirokov48331@yandex.ru>
2026-02-26 02:00:49 +0300

    модель званий

--- duty_flow/ranks/models.py
+++ duty_flow/ranks/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Rank(models.Model):
+    """Звание"""
+    name = models.CharField(max_length=100, unique=True, verbose_name="Звание")
+    order = models.PositiveSmallIntegerField(default=0, verbose_name="Порядок") # Для сортировки (рядовой -> генерал)
+
+    class Meta:
+        verbose_name = "Звание"
+        verbose_name_plural = "Звания"
+        ordering = ['order', 'name']
+
+    def __str__(self):
+        return self.name
