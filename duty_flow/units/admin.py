bb798fe7570de24cd85a3c990af620d93bb13e48
m48331 <maksshirokov48331@yandex.ru>
2026-02-27 00:43:56 +0300

    units

--- duty_flow/units/admin.py
+++ duty_flow/units/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Unit, UnitType
 
-# Register your models here.
+@admin.register(UnitType)
+class UnitTypeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    search_fields = ('name',)
+
+@admin.register(Unit)
+class UnitAdmin(admin.ModelAdmin):
+    list_display = ('name', 'unit_type', 'parent')
+    list_filter = ('unit_type',)
+    search_fields = ('name',)
