ac45b73b50ba7a3f8fce4605783cf4bf2db5cd4a
m48331 <maksshirokov48331@yandex.ru>
2026-02-27 00:46:13 +0300

    units

--- duty_flow/units/admin.py
+++ duty_flow/units/admin.py
@@ -5,9 +5,24 @@ from .models import Unit, UnitType
 class UnitTypeAdmin(admin.ModelAdmin):
     list_display = ('name', 'slug')
     search_fields = ('name',)
+    list_filter = ('name',)
+    
+    class Meta:
+        verbose_name = "Тип подразделения"
+        verbose_name_plural = "Типы подразделений"
 
 @admin.register(Unit)
 class UnitAdmin(admin.ModelAdmin):
     list_display = ('name', 'unit_type', 'parent')
     list_filter = ('unit_type',)
     search_fields = ('name',)
+    
+    fieldsets = (
+        ('Основная информация', {
+            'fields': ('name', 'unit_type')
+        }),
+        ('Иерархия', {
+            'fields': ('parent',),
+            'classes': ('wide',),
+        }),
+    )
