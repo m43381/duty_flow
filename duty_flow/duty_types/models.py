3a8c0bd706bc98cab6ce728e53a92f9ea1899a90
m48331 <maksshirokov48331@yandex.ru>
2026-02-26 23:11:14 +0300

    Добавил поле указывающее на подразделение, создавшее наряд

--- duty_flow/duty_types/models.py
+++ duty_flow/duty_types/models.py
@@ -1,10 +1,21 @@
 from django.db import models
+from units.models import Unit
 
 class DutyType(models.Model):
     """Тип наряда"""
     name = models.CharField(max_length=150, unique=True, verbose_name="Название")
     description = models.TextField(blank=True, verbose_name="Описание")
     required_people = models.PositiveSmallIntegerField(default=1, verbose_name="Требуется человек")
+    
+    # Поле указывающее на подразделение, создавшее этот тип наряда
+    created_by_unit = models.ForeignKey(
+        Unit,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='created_duty_types',
+        verbose_name="Создано подразделением"
+    )
 
     class Meta:
         verbose_name = "Тип наряда"
