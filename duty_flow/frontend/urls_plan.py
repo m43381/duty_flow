acaa625d41c50784666b1195cf686884249c8d17
m48331 <maksshirokov48331@yandex.ru>
2026-03-22 18:03:44 +0300

    новые маршруты

--- duty_flow/frontend/urls_plan.py
+++ duty_flow/frontend/urls_plan.py
@@ -4,13 +4,24 @@ from . import views_plan
 app_name = 'plan'
 
 urlpatterns = [
-    # Базовый CRUD
-    path('', views_plan.plan_list, name='list'),
-    path('add/', views_plan.plan_add, name='add'),
-    path('<int:pk>/', views_plan.plan_detail, name='detail'),
-    path('<int:pk>/edit/', views_plan.plan_edit, name='edit'),
-    path('<int:pk>/delete/', views_plan.plan_delete, name='delete'),
+    # Список расписаний
+    path('', views_plan.schedule_list, name='list'),
+    
+    # Создание расписания
+    path('add/', views_plan.schedule_add, name='add'),
+    
+    # Просмотр/редактирование расписания
+    path('<int:pk>/', views_plan.schedule_detail, name='detail'),
+    path('<int:pk>/edit/', views_plan.schedule_edit, name='edit'),
+    path('<int:pk>/delete/', views_plan.schedule_delete, name='delete'),
     
     # Редактирование дней (таблица)
-    path('<int:pk>/days/', views_plan.plan_days, name='days'),
+    path('<int:pk>/days/', views_plan.schedule_days, name='days'),
+    
+    # Входящие назначения
+    path('incoming/', views_plan.incoming_assignments, name='incoming'),
+    path('create-from-incoming/<int:day_plan_id>/', views_plan.create_from_incoming, name='create_from_incoming'),
+    
+    # API для обновления DayPlan
+    path('day/<int:day_plan_id>/update/', views_plan.day_update, name='day_update'),
 ]
